3d9231b84f878fbfeb141a9176e00666fda7a5ae
Greytiger23 <isabelmothoa49@gmail.com>
2023-11-30 13:18:37 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     a = len(c) - 1
     if a == 0:
         print("{:d} arguments.".format(a))
-    if a > 0:
+    if a > 1:
         print("{:d} arguments:".format(a))
         for b in range(1, a + 1):
             print("{:d}: {}".format(b, c[b]))
+    elif a > 0:
+        print("{:d} argument:".format(a))
+        print("{:d}: {}".format(a, c[1]))
