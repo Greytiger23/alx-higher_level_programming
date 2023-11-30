b286b5208d09eb3340b3634be3cf150a7ca2d3ae
Greytiger23 <isabelmothoa49@gmail.com>
2023-11-30 12:52:17 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
-a = len(argv) - 1
-if a == 0:
-    print("{:d} arguments.".format(a))
-if a > 0:
-    print("{:d} arguments:".format(a))
-    for b, arg in enumerate(argv[1:], start=1):
-        print("{:d}: {}".format(b, arg))
+if __name__ == "__main__":
+    from sys import argv
+    a = len(argv) - 1
+    if a == 0:
+        print("{:d} arguments.".format(a))
+    if a > 0:
+        print("{:d} arguments:".format(a))
+        for b, arg in enumerate(argv[1:], start=1):
+            print("{:d}: {}".format(b, arg))
