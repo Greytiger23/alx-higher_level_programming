14e29ed19e48b6dff3670a9727a9a7c7a74b9944
Greytiger23 <isabelmothoa49@gmail.com>
2023-11-30 17:29:15 -0800

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv, exit
-if len(argv) != 4:
-    print("Usage: {} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-a = int(argv[1])
-d = argv[2]
-b = int(argv[3])
-x = 0
-if d == '+':
-    x = add(a, b)
-elif d == '-':
-    x = sub(a, b)
-elif d == '*':
-    x = mul(a, b)
-elif d == '/':
-    x = div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-print("{:d} {:s} {:d} = {:d}".format(a, d, b, x))
-exit(0)
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    if len(argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    a = int(argv[1])
+    d = argv[2]
+    b = int(argv[3])
+    x = 0
+    if d == '+':
+        x = add(a, b)
+    elif d == '-':
+        x = sub(a, b)
+    elif d == '*':
+        x = mul(a, b)
+    elif d == '/':
+        x = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {:s} {:d} = {:d}".format(a, d, b, x))
+    exit(0)
