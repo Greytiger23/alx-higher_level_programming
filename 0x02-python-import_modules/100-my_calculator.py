22edc19275ff59618f24a655498ee21b9fd141a2
Greytiger23 <isabelmothoa49@gmail.com>
2023-11-30 17:20:24 -0800

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, sub, mul, div
 from sys import argv, exit
 if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    print("Usage: {} <a> <operator> <b>".format(argv[0]))
     exit(1)
 a = int(argv[1])
 d = argv[2]
