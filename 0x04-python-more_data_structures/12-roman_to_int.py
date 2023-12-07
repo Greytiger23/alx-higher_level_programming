70d0d6088d144224b68ecef71dadc93121229700
Greytiger23 <isabelmothoa49@gmail.com>
2023-12-06 17:28:41 -0800

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not str(roman_string) or roman_string is None:
+        return 0
+    a = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000}
+    x = 0
+    d = 0
+    for b in reversed(roman_string):
+        c = a[b]
+        if c < d:
+            x -= c
+        else:
+            x += c
+        d = c
+    return x
