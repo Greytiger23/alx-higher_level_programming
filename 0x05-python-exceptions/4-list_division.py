bd9f0a501a0819b8b61104e15408025ecdaf7222
Greytiger23 <isabelmothoa49@gmail.com>
2023-12-18 14:49:45 -0800

    python exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     x = []
-    for a in range(list_length):
+    for a in range(0, list_length):
         try:
             b = float(my_list_1[a])
             c = float(my_list_2[a])
