9e4ac00e10f0393e38de9a671b351d9e7ce48a27
Greytiger23 <isabelmothoa49@gmail.com>
2024-01-10 16:58:30 -0800

    python input output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -6,4 +6,4 @@ def append_write(filename="", text=""):
     """represents the write file"""
     with open(filename, "a", encoding="utf-8") as file:
         c = file.write(text)
-        return c 
+        return c
