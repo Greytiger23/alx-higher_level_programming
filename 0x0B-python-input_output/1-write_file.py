96185c54266f91ed597371840eb3437898699d23
Greytiger23 <isabelmothoa49@gmail.com>
2024-01-10 16:57:04 -0800

    python input output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,5 +5,4 @@
 def write_file(filename="", text=""):
     """represents the write file"""
     with open(filename, "w", encoding="utf-8") as file:
-        c = file.write(text)
-        return c 
+        return file.write(text)
