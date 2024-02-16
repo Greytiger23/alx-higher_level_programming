0af48e2afbfb8812eb04f6a9d023a845c707da1d
Greytiger23 <isabelmothoa49@gmail.com>
2024-02-15 18:28:05 -0800

    sql more queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,8 @@
 -- Lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genres_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
+GROUP BY tv_shows.title
 ORDER BY tv_shows.title ASC;
