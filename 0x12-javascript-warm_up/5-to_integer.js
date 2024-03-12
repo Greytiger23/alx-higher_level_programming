8f4aecfe3db9c546f6e4271d0fcf33b1ca372297
Greytiger23 <isabelmothoa49@gmail.com>
2024-03-11 17:22:00 -0700

    javascript warm up

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const a = process.argv[2];
+if (!isNaN(a))
+{
+console.log('My number:', parseInt(a));
+}
+else
+{
+console.log("Not a number");
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const c = [
+	"C is fun",
+	"Python is cool",
+	"JavaScript is amazing"
+];
+for (let a = 0; a < c.length; a++)
+{
+console.log(c[a]);
+}
