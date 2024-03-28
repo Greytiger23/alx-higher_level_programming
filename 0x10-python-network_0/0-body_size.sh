25b055adc5b3c174be523816b0c0d09b4e755e7f
Greytiger23 <isabelmothoa49@gmail.com>
2024-03-27 20:00:32 -0700

    python networking

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL
-curl -s "$1" | grep -i content-length | wc -c
+curl -sI "$1" | grep -i Content-Length | wc -c
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all
-curl -sX OPTIONS "$1" | grep -i Allow | cut -d ':' -f 2
+curl -sI -X OPTIONS "$1" | grep -i Allow | cut -d ':' -f 2
