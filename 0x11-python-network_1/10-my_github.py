1fe6b172d465d2a6c417cfececeed07b56c223cb
Greytiger23 <isabelmothoa49@gmail.com>
2024-03-28 20:43:11 -0700

    python network 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """takes url and sends request"""
+
 import requests
 import sys
 
-
-username = sys.argv[1]
-password = sys.argv[2]
-url = 'https://api.github.com/user'
-a = requests.get(url, auth=(username, password))
-if a.status_code == 200:
-    user_info = a.json()
-    print("Your GitHub user ID is:", user_info['id'])
-else:
-    print("Failed to retrieve user information. Status code:", a.status_code)
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    a = requests.get(url, auth=(username, password))
+    if a.status_code == 200:
+        user_info = a.json()
+        print(user_info['id'])
+    else:
+        print("None")
