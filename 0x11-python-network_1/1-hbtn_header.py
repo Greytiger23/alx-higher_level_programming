844d3f83cff9ad2433dd9626a1e649327e670185
Greytiger23 <isabelmothoa49@gmail.com>
2024-03-28 19:00:30 -0700

    python network 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ import urllib.request
 
 
 url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
+a = urllib.request.Request(url)
+with urllib.request.urlopen(a) as response:
+    b = response.headers.get('X-Request-Id')
+    print(b)
