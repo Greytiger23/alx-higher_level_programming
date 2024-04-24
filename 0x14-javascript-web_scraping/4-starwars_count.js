a241a09042ee58257f7d1f90ed5b2e49e1d1512a
Greytiger23 <isabelmothoa49@gmail.com>
2024-04-23 17:48:50 -0700

    javascript web scraping

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const fId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Error: Status code ' + response.statusCode);
+  }
+
+  const f = JSON.parse(body).results;
+  const count = f.reduce((total, film) => {
+    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/' + fId + '/')) {
+      return total + 1;
+    }
+    return total;
+  }, 0);
+
+  console.log(count);
+});
